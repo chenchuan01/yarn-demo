9aa161507e48cd6e79d7e73096b812cc8acb3264
chenchuan <329985581@qq.com>
2017-03-13 12:09:03 +0800

    [ADD] container dist

---
+++ yarn/yarn-dist/src/main/java/com/spring/yarn/dist/App.java
@@ -0,0 +1,13 @@
+package com.spring.yarn.dist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
